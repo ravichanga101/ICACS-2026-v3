6fee363dbae3dd608ca55ead80c752ec948cdb28
Ravi Patel <ravifromchanga@gmail.com>
2026-07-05 11:21:43 +0530

    deadline extended : to 20 July - Madam - whatsup

--- src/components/ImportantDates.jsx
+++ src/components/ImportantDates.jsx
@@ -4,7 +4,7 @@ export default function ImportantDates() {
   const importantDates = [
     {
       event: "Paper Submission Deadline",
-      date: "July 10, 2026",
+      date: "July 20, 2026",
       icon: (
         <svg
           xmlns="http://www.w3.org/2000/svg"
